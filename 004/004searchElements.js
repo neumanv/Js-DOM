cee3141d05a80ab0c745f007f89f0bb2e56bb581
neumanv <guillermoanv25@gmail.com>
2022-12-04 15:42:28 +0100

    Cambios

---
+++ 004/004searchElements.js
@@ -0,0 +1,10 @@
+"use strict"
+
+//La tabla con id="age-table"
+document.getElementById("age-table");
+
+//Todas los elementos label dentro de la tabla (debería haber 3)
+table.getElementsByTagName("label");
+
+//El primer td en la tabla (con la palabra “Age”)
+table.rows[0].cells[0];
