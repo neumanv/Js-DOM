6b94160325b7e92f1faa22c508d048b919a46fca
neumanv <guillermoanv25@gmail.com>
2022-12-04 16:58:54 +0100

    Hecho

--- 004/004searchElements.js
+++ 004/004searchElements.js
@@ -1,10 +1,20 @@
 "use strict"
 
 //La tabla con id="age-table"
-document.getElementById("age-table");
+let tabla = document.getElementById("age-table")
+console.log(tabla);
 
 //Todas los elementos label dentro de la tabla (debería haber 3)
-table.getElementsByTagName("label");
+console.log(document.getElementsByTagName("label"));
 
 //El primer td en la tabla (con la palabra “Age”)
-table.rows[0].cells[0];
+console.log(tabla.rows[0].cells[0]);
+
+//El form con name="search"
+console.log(document.getElementsByName("search"));
+
+//El primer input en el form
+console.log(document.getElementsByTagName("input")[0]);
+
+//El último input en ese formulario
+console.log(document.getElementsByTagName("input")[8]);
