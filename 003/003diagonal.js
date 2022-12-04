e684a4e8931ca32a21b999483650f04167abd721
neumanv <guillermoanv25@gmail.com>
2022-12-04 16:35:35 +0100

    Cambios

--- 003/003diagonal.js
+++ 003/003diagonal.js
@@ -1,4 +1,23 @@
 "use strict"
 
-let color = document.getElementById("color");
-color.style.backgroundColor = "red";
+let tabla = document.body.firstElementChild;
+
+/*let td1 = tabla.rows[0].cells[0];
+td1.style.backgroundColor = 'red';
+
+let td2 = tabla.rows[1].cells[1];
+td2.style.backgroundColor = 'red';
+
+let td3 = tabla.rows[2].cells[2];
+td3.style.backgroundColor = 'red';
+
+let td4 = tabla.rows[3].cells[3];
+td4.style.backgroundColor = 'red';
+
+let td5 = tabla.rows[4].cells[4];
+td5.style.backgroundColor = 'red';*/
+
+for(let i = 0; i > tabla.length; i++){
+    let color = tabla.rows[i].cells[i];
+    color.style.backgroundColor = "red";
+}
