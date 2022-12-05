2317cbfbeeb25030d4323a42f5828f3484640a1c
neumanv <guillermoanv25@gmail.com>
2022-12-05 09:26:19 +0100

    Por hacer

---
+++ 016/016paintClock.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let hora = document.getElementById("hours");
+hora = getHours();
+
+let minuto = document.getElementById("minutes");
+minuto = getMinutes();
+
+let segundo = document.getElementById("seconds");
+segundo = getSeconds();
+
+function empezar(){
+}
