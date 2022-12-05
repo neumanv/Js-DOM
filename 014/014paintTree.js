ea79dcf7c3f53aefa064e3c59a744ec1e6699f42
neumanv <guillermoanv25@gmail.com>
2022-12-05 09:58:02 +0100

    Sin resolver

---
+++ 014/014paintTree.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let data = {
+    "Fish": {
+        "trout": {},
+        "salmon": {}
+    },
+    "Tree": {
+        "Huge": {
+            "sequoia": {},
+            "oak": {}
+        },
+        "Flowering": {
+            "apple tree": {},
+            "magnolia": {}
+        }
+    }
+};
+
+let container = document.getElementById('container');
+
+function createTree(){
+    
+}
+
+createTree(container, data); // crea el árbol en el contenedor
