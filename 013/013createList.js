2e968a673e14a0997e6014eba47fea805fad0e60
neumanv <guillermoanv25@gmail.com>
2022-12-07 12:19:47 +0100

    Cambios

--- 013/013createList.js
+++ 013/013createList.js
@@ -1,1 +1,18 @@
 "use strict"
+
+while(true){
+
+    let obj = prompt("Añadir a la lista");
+
+    if(obj === null){
+        break;
+    }else{
+
+        let ul = document.createElement("ul");
+        document.body.appendChild(ul);
+        
+        let li = document.createElement("li");
+        ul.appendChild(li);
+        li.innerHTML = obj;
+    }
+}
