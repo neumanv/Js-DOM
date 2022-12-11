2276e5d2a45dba58d358aa6245e07d4710712b56
neumanv <guillermoanv25@gmail.com>
2022-12-11 13:06:05 +0100

    Incompleto

--- 030/030shortOuts.js
+++ 030/030shortOuts.js
@@ -1,1 +1,16 @@
 "use strict"
+
+function pulsado(){
+
+    let presionado = new Set()  //Set() es un objeto que permite almacenar valores
+
+    document.addEventListener("keydown", function(event) {
+        presionado.add(event.code);
+    });
+}
+
+runOnKeys(
+    () => alert("Hello!"),
+    "KeyQ",
+    "KeyW"
+);
