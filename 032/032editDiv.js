f4a52c66121484b4c71a44eda18c05b69da76cfd
neumanv <guillermoanv25@gmail.com>
2022-12-11 14:20:33 +0100

    Sin terminar

---
+++ 032/032editDiv.js
@@ -0,0 +1,9 @@
+"use strict"
+
+document.getElementById("cambiar").addEventListener("click", function(){
+    document.getElementById("divcambiar").innerHTML = "<textarea></textarea>";
+});
+
+/*document.getElementsByTagName("textarea").addEventListener("blur", function(){
+    document.getElementsByTagName("textarea").innerHTML = "<div></div>";
+});*/
