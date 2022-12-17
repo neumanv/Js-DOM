8a4f3ed0fc87e86ce79e1dd1a650b82dec1986ca
neumanv <guillermoanv25@gmail.com>
2022-12-17 14:00:38 +0100

    Faltar pulir

---
+++ 031/031addOption.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let genres = document.getElementById("genres");
+
+let nuevo = new Option("option");
+nuevo.value = "flamenco";
+nuevo.text = "Flamenco";
+
+genres.appendChild(nuevo); 
