1990158a0472518452fbc74701dfdddd693b6653
neumanv <guillermoanv25@gmail.com>
2022-12-19 13:22:22 +0100

    Cambiando

--- 018/018orderTable.js
+++ 018/018orderTable.js
@@ -1,1 +1,8 @@
 "use strict"
+
+let tabla = document.getElementsByTagName("table");
+
+let sortedRows = Array.from(tabla.tBodies[0].rows) // 1
+  .sort((rowA, rowB) => rowA.cells[0].innerHTML.localeCompare(rowB.cells[0].innerHTML));
+
+  tabla.tBodies[0].append(...sortedRows);
