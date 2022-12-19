98868fbfa0672a33928a2afc1ceb882eae6b068d
neumanv <guillermoanv25@gmail.com>
2022-12-19 13:13:30 +0100

    Incompleto

--- 016/016paintClock.js
+++ 016/016paintClock.js
@@ -1,13 +1,30 @@
 "use strict"
 
-let hora = document.getElementById("hours");
-hora = getHours();
+let SPAN_HOURS = document.querySelector('span#hours');
+let SPAN_MINUTES = document.querySelector('span#minutes');
+let SPAN_SECONDS = document.querySelector('span#seconds');
 
-let minuto = document.getElementById("minutes");
-minuto = getMinutes();
+//Cálculos en ms (millisegundos)
+let MILLISECONDS_OF_A_SECOND = 1000;
+let MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+let MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+let MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
 
-let segundo = document.getElementById("seconds");
-segundo = getSeconds();
+//FUNCIONES
 
-function empezar(){
+function start() {
+    //Cálculos de tiempo
+    let NOW = new Date()
+    let DURATION = DATE_TARGET - NOW;
+    let REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    let REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    let REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
 }
+
+updateCountdown();
+//Reiniciar cada segundo
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
