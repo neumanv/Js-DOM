2a7e9bb08a84fa67e53cffc25183d09a6c70ca68
neumanv <guillermoanv25@gmail.com>
2022-12-19 19:02:14 +0100

    Hecho

--- 030/030shortOuts.js
+++ 030/030shortOuts.js
@@ -1,16 +1,27 @@
 "use strict"
 
-function pulsado(){
+function runOnKeys(func, ...codes) {
+    let pulsado = {};
 
-    let presionado = new Set()  //Set() es un objeto que permite almacenar valores
+    document.addEventListener("keydown", function (event){
+        pulsado[event.code] = true;
 
-    document.addEventListener("keydown", function(event) {
-        presionado.add(event.code);
+        //Comprobar si las teclas están pulsadas
+        if (codes.every(function (code) {
+            return pulsado[code];
+        })){
+            func();
+        }
+    });
+
+    // Agregar un manejador de eventos keyup al documento
+    document.addEventListener("keyup", function (event){
+        pulsado[event.code] = false;
     });
 }
 
 runOnKeys(
-    () => alert("Hello!"),
+    () => alert("¡Hola!"),
     "KeyQ",
     "KeyW"
 );
