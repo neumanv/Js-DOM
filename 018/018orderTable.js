c00c7e37ac468e8c345036cb14b75ee6b73211f7
neumanv <guillermoanv25@gmail.com>
2022-12-19 13:23:32 +0100

    Cambiando

--- 018/018orderTable.js
+++ 018/018orderTable.js
@@ -1,8 +1,7 @@
 "use strict"
 
-let tabla = document.getElementsByTagName("table");
 
-let sortedRows = Array.from(tabla.tBodies[0].rows) // 1
-  .sort((rowA, rowB) => rowA.cells[0].innerHTML.localeCompare(rowB.cells[0].innerHTML));
+let sortedRows = Array.from(table.tBodies[0].rows)
+.sort((rowA, rowB) => rowA.cells[0].innerHTML.localeCompare(rowB.cells[0].innerHTML));
 
-  tabla.tBodies[0].append(...sortedRows);
+  table.tBodies[0].append(...sortedRows);
